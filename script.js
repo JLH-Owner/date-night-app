496d8c2a79d4133ad0db5c7059fb741ee0dce9ee
Stacy <jlhclientsolutions@gmail.com>
2024-06-13 20:05:49 -0400

    added modal for instructions to html and js file

--- script.js
+++ script.js
@@ -2,6 +2,31 @@ const recipeButton = document.querySelector('#recipe-button');
 const resultList = document.querySelector('#results');
 const genreInput = document.querySelector('#genre');
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Functions to open and close a modal
+    function openModal($el) {
+      $el.classList.add('is-active');
+    }
+  
+    function closeModal($el) {
+      $el.classList.remove('is-active');
+    }
+  
+    function closeAllModals() {
+      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+        closeModal($modal);
+      });
+    }});
+
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+    
+        $trigger.addEventListener('click', () => {
+          openModal($target);
+        });
+      });
+
 recipeButton.addEventListener('click', function(event) {
 event.preventDefault();
 
